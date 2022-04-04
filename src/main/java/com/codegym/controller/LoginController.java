2f1660c7f5ed3d443e3a6c9e3e811cc4669c0d1d
LuytBui <luytbq43@gmail.com>
2022-04-04 10:16:25 +0700

    first commit

---
+++ src/main/java/com/codegym/controller/LoginController.java
@@ -0,0 +1,17 @@
+package com.codegym.controller;
+
+import com.codegym.model.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/login")
+public class LoginController {
+
+    @GetMapping
+    public ModelAndView loginPage(){
+        return new ModelAndView("/login", "user", new User());
+    }
+}
